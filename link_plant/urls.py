fedb0f40b7254a422aedda2ca279aaca59543b06
Kudhan <kudhanshaik03@gmail.com>
2025-09-06 15:48:22 +0530

    completed

--- link_plant/urls.py
+++ link_plant/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import LinkListView, LinkCreateView, UpdateLinkView
+from .views import LinkListView, LinkCreateView, UpdateLinkView,DeleteLinkView,profile_view
 
 urlpatterns = [
-    path('', LinkListView.as_view(), name='link-list'),
+    path('manage/', LinkListView.as_view(), name='link-list'),
     path('create/', LinkCreateView.as_view(), name='create-list'),
     path('<int:pk>/update/', UpdateLinkView.as_view(), name='update-list'),
+    path('<int:pk>/delete/', DeleteLinkView.as_view(), name='delete-list'),
+    path('<slug:profile_slug>', profile_view, name='profile'),
 ]
--- link_plant/views.py
+++ link_plant/views.py
@@ -1,6 +1,8 @@
-from django.views.generic import ListView, CreateView, UpdateView
+from django.views.generic import ListView, CreateView, UpdateView,DeleteView
 from django.urls import reverse_lazy
-from .models import Link
+from django.shortcuts import render, get_object_or_404
+
+from .models import Link,Profile
 
 class LinkListView(ListView):
     model = Link
@@ -17,3 +19,18 @@ class UpdateLinkView(UpdateView):
     fields = ['text', 'url']                      # only editable fields
     template_name = "link_plant/link_form.html"  # reuse same form
     success_url = reverse_lazy('link-list')       # redirects to list after update
+
+class DeleteLinkView(DeleteView):
+    model = Link
+    success_url = reverse_lazy('link-list')       # redirects to list after delete
+    template_name = "link_plant/link_confirm_delete.html"  # confirm delete template
+
+def profile_view(request,profile_slug):
+    profile = get_object_or_404(Profile, slug=profile_slug)
+    links=profile.links.all()
+    context={
+        'profile':profile,
+        'links':links
+    }
+
+    return render(request,'link_plant/profile.html',context)
